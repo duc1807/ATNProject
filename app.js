69cade4d6153e87187ba7f38b16e9aa123201b01
duc1807 <62170104+duc1807@users.noreply.github.com>
2020-04-18 20:12:49 +0700

    Up lai port sau khi chay duoc local

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
-//app.listen(process.env.port || 3000);   
+app.listen(process.env.port || 3000);   
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,5 +33,3 @@ var employeeController = require("./employee.js");
 app.use('/', indexController);
 app.use('/homepage', homepageController);
 app.use('/employeepage', employeeController);
-
-var server = app.listen(3000, function() {});
